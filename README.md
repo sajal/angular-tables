8f41bf8c18550d3a8de288b8b7b3965ba9990908
Sajal Kayan <sajal83@gmail.com>
2012-10-07 21:15:49 +0700

    dynamic js table configurable using json

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+WORK IN PROGRESS DO NOT USE!
+
 angular-tables
 ==============
 
-Attempt to make fancy tables based on angular.js with non-js fallback
+Attempt to make fancy tables based on angular.js with non-js fallback.
+
+We get tempted to use rich JavaScript tables (like Google visualizations) in blogposts, completely ignoring non-js clients such as RSS readers, blog-to-email functions, etc. This project is an attempt to show static HTML which gets replaced by JS table id possible without having to write a lot of code each time.
 
 Scope
 =====
---
+++ directives/table.js
@@ -0,0 +1,130 @@
+angular.module('AngularTable', []).directive('angulartable', function ($filter) {
+  return {
+    restrict:'EAC',
+    replace:true,
+    templateUrl:'/directives/template.html',
+    scope: {"tdata" : "=ngModel"},
+    link:function (scope, element, attrs) {
+      scope.getsortclass = function(key, reverse){
+        return (scope.sorting.key==key && scope.sorting.reverse==reverse) ? "sortactive":"sortinactive"
+      }
+      scope.getfiltered = function(item, key){
+        var filtered = item;
+        if (scope.tdata.config && scope.tdata.config.filters && scope.tdata.config.filters[key]){
+          
+          if (scope.tdata.config.filters[key].number || scope.tdata.config.filters[key].number == 0){
+            //need to check for undefined cause number can be 0
+            filtered = $filter('number')(filtered, scope.tdata.config.filters[key].number);
+          }
+          
+          if (scope.tdata.config.filters[key].after){
+            filtered = filtered + scope.tdata.config.filters[key].after
+          }
+        }
+        return filtered
+      }
+      var initialize = function(){
+        //Runs each time the associated ng-model is updated
+        if (scope.tdata){
+          //We has tdata
+          scope.colkeys = [];
+          scope.head = [];
+          scope.search = {};
+          var row1 = [], 
+            row2=[],
+            hasrow2=false;
+          angular.forEach(scope.tdata.columns, function(item){
+            //for each main column
+            var cols = 1;
+            if (item.subcols){
+              hasrow2 = true;
+              cols = item.subcols.length;
+              angular.forEach(item.subcols, function(subcol){
+                //for each subcolumn
+                this.push({
+                  label: subcol.name,
+                  colspan: 1,
+                  key: subcol.key,
+                  sortable: subcol.sortable,
+                  searchable: subcol.searchable
+                })
+                scope.colkeys.push(subcol.key);
+              }, row2);
+            } else {
+              row2.push({name: "", colspan: 1})
+              scope.colkeys.push(item.key);
+            }
+            this.push({
+              label: item.name,
+              colspan: cols,
+              key: item.key,
+              sortable: item.sortable,
+              searchable: item.searchable
+            })
+          }, row1);
+          scope.head.push(row1);
+          if (hasrow2){
+            scope.head.push(row2);
+          }
+          scope.original = scope.tdata.rows;
+          console.log(scope.head, scope.colkeys)
+        }
+        if ((scope.tdata.config) && (scope.tdata.config.defaultsort)){
+          scope.sorting = scope.tdata.config.defaultsort
+        } else {
+          scope.sorting = {}
+        }
+        //Bootstrap pagination things
+        if ((scope.tdata.config) && (scope.tdata.config.perpage)){
+          scope.perpage = scope.tdata.config.perpage
+          console.log(scope.perpage)
+        } else {
+          scope.perpage = 20
+        }
+        scope.refilter(1)
+      }
+
+      scope.refilter = function(pagenum){
+        scope.curpage = pagenum
+        console.log(pagenum)
+        scope.body = $filter('filter')(scope.original, scope.search);
+        if (scope.sorting && scope.sorting.key){
+          scope.body.sort(function(a,b){
+            return a[scope.sorting.key] - b[scope.sorting.key]
+          });
+          if (scope.sorting.reverse){
+            scope.body.reverse()
+          }
+        }
+        scope.numpages = Math.ceil(scope.body.length / scope.perpage);
+        //Set class by hand- dunno why angular gets confused :(
+        var tmpfiltered = scope.body.slice(scope.perpage * (scope.curpage - 1))
+        scope.body = []
+        var rowclass = "even"
+        angular.forEach(tmpfiltered, function(row){
+          rowclass = rowclass == "even" ? "odd":"even" ;
+          row._cssclass = rowclass;
+          this.push(row)
+        }, scope.body)
+        
+      }
+
+
+
+      //run initialize once.. just for the fun of it..
+      initialize()
+      scope.$watch("ngModel", function(){
+        //The whole table gets re-initialized if associated data changes
+        initialize()
+      })
+      scope.$watch("search", function(){
+        //Rerun all the filters if search changes, goto page 1
+        scope.refilter(1)
+      }, true)
+      scope.$watch("sorting", function(){
+        //Rerun all the filters if sorting conditions change, goto page 1
+        scope.refilter(1)
+      }, true)
+    }
+  }
+})
